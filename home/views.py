004801c4aa699b95d45c08a3db528ff4a659e3c5
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2025-08-05 21:48:38 +0330

    feat: Restore bestsellers context in HomeView and update product detail URLs

--- home/views.py
+++ home/views.py
@@ -12,7 +12,7 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         categories = Category.objects.filter(is_active=True, parent__isnull=True)
         context['categories'] = categories
-        # context['bestsellers'] = Product.objects.filter(is_available=True).annotate(
-        #     total_sold=Sum('orderitem__quantity')
-        # ).order_by('-total_sold')[:5]
+        context['bestsellers'] = Product.objects.filter(is_available=True).annotate(
+            total_sold=Sum('orderitem__quantity')
+        ).order_by('-total_sold')[:5]
         return context
